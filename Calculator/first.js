dce2f0ac12a4339548156a7801b12be79094d6e6
wilsonlogo <2023ugcs019@nitjsr.ac.in>
2026-07-07 01:06:39 +0530

    correct input element IDs and enhance calculator functionality

--- Calculator/first.js
+++ Calculator/first.js
@@ -1,4 +1,16 @@
 const button =document.querySelector("button");
 button.addEventListener("click",()=>{
-    let number1=document.getElementById("firstnumber");
+    let input1=document.getElementById("firstNumber");
+    let number1=Number(input1.value);
+    
+     let input2=document.getElementById("secondNumber");
+    let number2=Number(input2.value);
+
+    if(isNaN==number1 || isNaN==number2){
+        return ;
+    }
+
+    let result=number1+number2;
+    let re=document.getElementById("result");
+    re.textContent=result;
 })
